10b9a41f68ab22283ae47ac5b136a489d2eef066
Thomas A. Drexl <tom.drexl@gmx.de>
2002-12-30 20:47:11 +0000

    - initial CVS import

---
+++ src/common.cpp
@@ -0,0 +1,26 @@
+/* This file includes all global definitions. */
+
+#ifndef IS_COMMON_H
+
+#include <string.h>
+#include <ctype.h>
+
+#include "common.h"
+
+/* Display text on stdout/stderr */
+void display (char *str, int level)
+{
+  int len = strlen (str);
+  if (!len) return;
+  if (level <= debuglevel)
+  {
+    if (level == LOG_NONE) fprintf (stdout, "%s\n", str);
+    else if (level == LOG_FATAL) fprintf (stderr, "Fatal: %s\n", str);
+    else if (level == LOG_ERROR) fprintf (stderr, "Error: %s\n", str);
+    else if (level == LOG_WARN) fprintf (stderr, "Warning: %s\n", str);
+    else if (level == LOG_MOST) fprintf (stdout, "Info: %s\n", str);
+    else fprintf (stdout, "Debug: %s\n", str);
+  }
+}
+
+#endif
